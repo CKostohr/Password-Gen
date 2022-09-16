2ce76dba5831127d05fbc5d56b71791ff41bccac
Chad Kostohryz <kostc7@gmail.com>
2022-09-15 21:26:01 -0500

    Finished works want to continue to see what else i can play with

--- Develop/script.js
+++ Develop/script.js
@@ -2,70 +2,90 @@
 
 
 // Get references to the #generate element
-let generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
+
+function randomInt(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var rand = Math.random()
+  return Math.floor(min * (1 - rand) + rand * max)
+}
+
+function getRandomItem(list) {
+  return list[randomInt(0, list.length)]
+}
 
 function generatePassword() {
 
-let userInput = window.prompt("How long do you need the password?")
+  var userInput = window.prompt("How long do you need the password?")
 
-let passwordLength = parseInt(userInput)
+  var passwordLength = parseInt(userInput)
 
 
-if (isNaN(passwordLength)) {
-  window.alert("That's not a number!")
-  return
-}
+  if (isNaN(passwordLength)) {
+    window.alert("That's not a number!")
+    return
+  }
 
-if (passwordLength < 8 || passwordLength > 128) {
-  window.alert("Password length MUST be between 8 and 128 characters!")
-  return
-}
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password length MUST be between 8 and 128 characters!")
+    return
+  }
 
-let wantsNumbers = window.confirm("Would you like to include numbers in your password?")
-let wantsSymbols = window.confirm("would you like to include symbols in your password?")
-let wantsLowercase = window.confirm("Would you like to include lowercase letters in you your password?")
-let wantsUppercase = window.confirm("Would you like to include uppercase letters in your password?")
+  var wantsNumbers = window.confirm("Would you like to include numbers in your password?")
+  var wantsSymbols = window.confirm("would you like to include symbols in your password?")
+  var wantsLowercase = window.confirm("Would you like to include lowercase letters in you your password?")
+  var wantsUppercase = window.confirm("Would you like to include uppercase letters in your password?")
 
-let numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-let symbolsList = ["!", "@", "#", "$", "%", "^", "&", "*"]
-let lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-let uppercaseList = []
+  var numbersList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var symbolsList = ["!", "@", "#", "$", "%", "^", "&", "*"]
+  var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uppercaseList = []
 
-let optionsCart = []
+  var optionsCart = []
 
-for (let i = 0; i < lowercaseList.length; i++) {
-  uppercaseList [i] = lowercaseList[i].toUpperCase()
-}
+  for (var i = 0; i < lowercaseList.length; i++) {
+    uppercaseList[i] = lowercaseList[i].toUpperCase()
+  }
 
-if (wantsNumbers === true) {
-  optionsCart.push(numberList)
-}
+  if (wantsNumbers === true) {
+    optionsCart.push(numbersList)
+  }
 
-if (wantsSymbols === true) {
-  optionsCart.push(symbolsList)
-}
+  if (wantsSymbols === true) {
+    optionsCart.push(symbolsList)
+  }
 
-if (wantsLowercase === true) {
-  optionsCart.push(lowercaseList)
-}
+  if (wantsLowercase === true) {
+    optionsCart.push(lowercaseList)
+  }
 
-if (wantsUppercase === true) {
-  optionsCart.push(uppercaseList)
-}
+  if (wantsUppercase === true) {
+    optionsCart.push(uppercaseList)
+  }
 
-let generatePassword = ""
+  if (optionsCart.length === 0) {
+    optionsCart.push(lowercaseList)
+  }
 
-for (let i = 0; i < passwordLength; i++) {
-  let randomList = getRandomItem(optionsCart)
-}
+  var thisIsWhereMagicHappens = ""
 
+  for (var i = 0; i < passwordLength; i++) {
+    var randomList = getRandomItem(optionsCart)
+    var randomChar = getRandomItem(randomList)
+    thisIsWhereMagicHappens += randomChar
+  }
+
+  return thisIsWhereMagicHappens
 }
 
 
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
